ed2aebf1778992566fa48738ba3039120e1e1e7f
Tai Pham <pham_ba_tai@sezax.co.jp>
2020-03-13 11:52:14 +0700

    Updates

---
+++ precache-manifest.a7d89c745af446371a946702c1af503a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebeaf57cf524537621680c74db511055",
+    "url": "/bataimx/interview-home-work/index.html"
+  },
+  {
+    "revision": "2743ef735abcb06d5f30",
+    "url": "/bataimx/interview-home-work/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "70b1fffa401647ed8c1a",
+    "url": "/bataimx/interview-home-work/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "2743ef735abcb06d5f30",
+    "url": "/bataimx/interview-home-work/static/js/2.41b44ee6.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/bataimx/interview-home-work/static/js/2.41b44ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b1fffa401647ed8c1a",
+    "url": "/bataimx/interview-home-work/static/js/main.946557f2.chunk.js"
+  },
+  {
+    "revision": "6e9e2af530600be78f8f",
+    "url": "/bataimx/interview-home-work/static/js/runtime-main.4a103f53.js"
+  }
+]);
